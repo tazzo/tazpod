e9b066711fcc61f76819b3e46b104c64f0b2e1d3
tazzo <roberto.tazzoli@gmail.com>
2026-01-18 17:22:09 +0100

    feat: TazPod v4.0 - Migration to Ubuntu 24.04, Full IDE stack, and Starship Pastel preset

--- dotfiles/.config/nvim/lua/config/options.lua
+++ dotfiles/.config/nvim/lua/config/options.lua
@@ -1,17 +1,3 @@
--- Configurazione Clipboard OSC 52 per Neovim 0.10+
-if vim.fn.has('nvim-0.10') == 1 then
-  vim.g.clipboard = {
-    name = 'OSC 52',
-    copy = {
-      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-    },
-    paste = {
-      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-    },
-  }
-end
-
--- Usa unnamedplus per sincronizzare sempre con la clipboard di sistema
+-- Configurazione Standard Clipboard
+-- Neovim 0.10 rileverà automaticamente xclip se DISPLAY è impostato
 vim.opt.clipboard = "unnamedplus"
